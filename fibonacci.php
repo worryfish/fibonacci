efac9f6a4cdba5267ed04d3e09e449f640572044
breeze <breeze@worryfish.com>
2016-12-05 17:56:16 +0300

    pure php implementation, same speed as extension :)

--- fibonacci.php
+++ fibonacci.php
@@ -1,5 +1,19 @@
 <?php
-
 $count_number = isset($argv[1]) ? (int)$argv[1] : 1;
+
+function fibonacci_pure($n){
+  $a = 0;
+  $b = 1;
+
+  for ($i = 0; $i < $n; $i++){
+    $sum = $a+$b;
+    $a = $b;
+    $b = $sum;
+  }
+
+  return $a;
+}
+
+print fibonacci_pure($count_number).PHP_EOL;
 
 print fibonacci($count_number).PHP_EOL;
